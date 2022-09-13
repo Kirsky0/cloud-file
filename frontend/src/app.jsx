e1987672000cae28c1ba5637d0c912c8cf455d04
kirsk <343893081@qq.com>
2022-09-13 17:04:58 +0800

    add page form

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,16 +1,25 @@
-import React, {useState} from 'react';
+import {TextField, Button, FormControl, InputLabel, Input, FormHelperText, FormGroup} from "@material-ui/core";
+import React from 'react';
 import {manager} from "../declarations/manager"
+import {Controller, useForm} from "react-hook-form";
+
 
 const App = () => {
 
-    const handleFiles = (file) => {
-        console.log(file)
-        // let str = file.base64.toString()
-        // // manager.set_wasm('data:application/wasm;base64,AGFzbQEAAAAFg4CAgAABABAGkYCAgAACfwBBgIDAAAt/AEGAgMAACwelgICAAAMGbWVtb3J5AgAKX19kYXRhX2VuZAMAC19faGVhcF9iYXNlAwE=')
-        // manager.upload_module(str).then(r => {
-        //     console.log(r)
-        // })
-    }
+    const {control, handleSubmit, reset, setValue,} = useForm({
+        defaultValues: {
+            module_name: "",
+            bytecode: new Uint8Array(),
+            remark: ""
+        }
+    });
+    const onSubmit = (data) => {
+
+        console.log(new Date().getTime())
+        console.log(data)
+    };
+
+
     const handleFile = (e) => {
         const file = e.target.files[0]
         // manager.upload_module(file).then(r => {
@@ -25,34 +34,81 @@ const App = () => {
                 console.log(r)
             })
         }
-        //
-        // console.log(uint8Array)
     }
+
+    const uploadFile = (e) => {
+        const file = e.target.files[0]
+        // manager.upload_module(file).then(r => {
+        //     console.log(r)
+        // })
+        const reader = new FileReader()
+        reader.readAsArrayBuffer(file)
+
+        reader.onload = function () {
+            let uint8Array = new Uint8Array(reader.result);
+            console.log(uint8Array)
+            setValue("bytecode", uint8Array)
+        }
+    }
+
     return (
-
         <div>
-            {/*<input type="file" name="file" onChange={handleChange}/>*/}
             {/*<div>*/}
-            {/*    <button onClick={handleSubmission}>Submit</button>*/}
+            {/*    <input type="file" accept=".wasm" onChange={e =>*/}
+            {/*        handleFile(e)}/>*/}
             {/*</div>*/}
-            <div>
-                <input type="file" accept=".wasm" onChange={e =>
-                    handleFile(e)}/>
-            </div>
 
-            {/*<ReactFileReader*/}
 
-            {/*    fileTypes={[".wasm"]}*/}
-            {/*    base64*/}
-            {/*    handleFiles={handleFiles}>*/}
-            {/*    <button>*/}
-            {/*        选择文件*/}
-            {/*    </button>*/}
-            {/*</ReactFileReader>*/}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    name="module_name"
+                    control={control}
+                    render={({field}) => (
+                        <FormControl>
+                            <InputLabel htmlFor="module name">module name</InputLabel>
+                            <Input
+                                {...field}
+                                label="module name"
+                                variant="standard"/>
+                        </FormControl>
 
+                    )}
+                />
+                <Controller
+                    name="bytecode"
+                    control={control}
+                    render={({field: {value, onChange}}) => (
+                        <FormGroup>
+                            <TextField
+                                label="wasm file"
+                                type="file"
+                                onChange={uploadFile}
+                                variant="standard"/>
+                        </FormGroup>
+                    )}
+                />
+                <Controller
+                    name="remark"
+                    control={control}
+                    render={({field}) => (
+                        <FormControl>
+                            <InputLabel htmlFor="remark">remark</InputLabel>
+                            <Input
+                                {...field}
+                                label="remark"
+                                variant="standard"/>
+                        </FormControl>
+                    )}
+                />
+                <FormControl>
+                    <Button onClick={handleSubmit(onSubmit)} variant={"outlined"}>Submit</Button>
+                    <Button onClick={() => reset()} variant={"outlined"}>Reset</Button>
+                </FormControl>
+            </form>
         </div>
 
     )
 }
+
 
 export default App;
